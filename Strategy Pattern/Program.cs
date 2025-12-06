5d53851b370b94f415ce58624ec37a01fbc64d2a
DESKTOP-RNS2QJ2\azizu <azizulcsebsmrstu@gmail.com>
2025-12-06 11:38:27 +0600

    Strategy Design Pattern

---
+++ Strategy Pattern/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * The Strategy design pattern is a behavioral design pattern that enables selecting an algorithm or behavior at runtime.
+ * It is based on the principle of composition over inheritance.
+ */
+public interface IFlyBehavior
+{
+    void Fly();
+}
+
+public class FlyWithWings : IFlyBehavior
+{
+    public void Fly()
+    {
+        Console.WriteLine("I'm flying with wings!");
+    }
+}
+
+public class FlyNoWay : IFlyBehavior
+{
+    public void Fly()
+    {
+        Console.WriteLine("I can't fly.");
+    }
+}
+
+public abstract class Duck
+{
+    protected IFlyBehavior _flyBehavior;
+    public abstract void Display();
+}
+
+public class MountainDuck : Duck
+{
+    public MountainDuck()
+    {
+        _flyBehavior = new FlyWithWings();
+    }
+    public override void Display()
+    {
+        Console.WriteLine("I'm a Mountain Duck.");
+        _flyBehavior.Fly();
+    }
+}
+
+public class VillageDuck : Duck
+{
+    public VillageDuck()
+    {
+        _flyBehavior = new FlyNoWay();
+    }
+    public override void Display()
+    {
+        Console.WriteLine("I'm a Village Duck.");
+        _flyBehavior.Fly();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Duck mountainDuck = new MountainDuck();
+        mountainDuck.Display();
+
+        Console.WriteLine();
+        Duck villageDuck = new VillageDuck();
+        villageDuck.Display();
+    }
+}

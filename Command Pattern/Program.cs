eb09c5a06a56d1865c3e4a21d5cd362b8bf1ba26
DESKTOP-RNS2QJ2\azizu <azizulcsebsmrstu@gmail.com>
2025-12-07 21:51:25 +0600

    Command Pattern

--- Command Pattern/Program.cs
+++ Command Pattern/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * 
+ * The Command Pattern is a behavioral design pattern that converts actions into separate command objects,
+ * allowing the invoker to store, queue, and execute them later without knowing how each action is performed.
+ * 
+*/
+public interface ICommand
+{
+    void Execute();
+}
+
+public class LightOnCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("The light is ON");
+    }
+}
+
+public class LightOffCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("The light is OFF");
+    }
+}
+public class WakeUpEarlyCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Waking up early in the morning!");
+    }
+}
+
+public class Invoker
+{
+    private List<ICommand> commands = new();
+    public void AddCommand(ICommand command)
+    {
+        commands.Add(command);
+    }
+
+    public void ExecuteCommands()
+    {
+        foreach (var command in commands)
+        {
+            command.Execute();
+        }
+        commands.Clear();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Invoker invoker = new Invoker();
+        var lightOnCommand = new LightOnCommand();
+        var lightOffCommand = new LightOffCommand();
+        var wakeUpEarlyCommand = new WakeUpEarlyCommand();
+
+        invoker.AddCommand(lightOnCommand);
+        invoker.AddCommand(wakeUpEarlyCommand);
+        invoker.AddCommand(lightOffCommand);
+
+        invoker.ExecuteCommands();
+    }
+}
+
+
+
+
+
+
+

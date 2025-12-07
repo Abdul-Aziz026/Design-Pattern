c39d3a388c5e040f9c4072cde5e16ca3e6cb2120
DESKTOP-RNS2QJ2\azizu <azizulcsebsmrstu@gmail.com>
2025-12-07 22:38:34 +0600

    mediator pattern definition

--- Mediator Design Pattern/Program.cs
+++ Mediator Design Pattern/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * 
+ * Don’t let objects talk to each other directly. Instead, let a mediator object manage all communication between them.
+ * So objects become less dependent, more decoupled, and the system becomes easier to maintain.
+ * 
+ * Example:-
+ * ✈ Planes never talk to each other directly.
+ * 📡 All communication goes through the control tower (mediator).
+ * who can land, who can take off and who must wait
+ * 
+*/

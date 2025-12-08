88abb30ecc8496809bb1c0509ef9d2f9ae9c83bd
DESKTOP-RNS2QJ2\azizu <azizulcsebsmrstu@gmail.com>
2025-12-08 08:49:07 +0600

    Locator Service Design Pattern

--- Locator Design Pattern/Program.cs
+++ Locator Design Pattern/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public interface ILogger
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine("Console logger...");
+    }
+}
+
+public static class ServiceLocator
+{
+    private static Dictionary<object, object> _services = new();
+
+    public static void Registor<T>(T service)
+    {
+        _services.TryAdd(typeof(T), service);
+    }
+
+    public static T? GetService<T>()
+    {
+        try
+        {
+            return (T)_services[typeof(T)];
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"Service not found...");
+            return default(T);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ServiceLocator.Registor<ILogger>(new ConsoleLogger());
+
+        ILogger loggerService = ServiceLocator.GetService<ILogger>();
+        loggerService.Log("hello");
+    }
+}
+
+
